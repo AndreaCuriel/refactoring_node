3307b159e19c8860dc12736104c2fb27cc162f39
AndreaCuriel <acurielipn@hotmail.com>
2022-04-26 18:26:30 -0500

    Prueba 2 de ExplorerService en método getAmountOfExplorersByMission, fallida

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -1,10 +1,20 @@
 const ExplorerService = require("./../lib/services/ExplorerService");
 
 describe("ExplorerService test", () => {
-  test("1) test filter of the filterByMission", () => {
+  test("2) test filter of the filterByMission", () => {
     const explorer = [{ mission: "node" }];
     const filterExplorer = ExplorerService.filterByMission(explorer, "node");
 
     expect(filterExplorer.length).toBe(1);
+  });
+
+  test("1) test method of the getAmountOfExplorersByMission", () => {
+    const explorer = [{ mission: "node" }, { mission: "node" }];
+    const filterExplorer = ExplorerService.getAmountOfExplorersByMission(
+      explorer,
+      "node"
+    );
+
+    expect(filterExplorer).toBe(1);
   });
 });
