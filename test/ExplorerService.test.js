e10de595f1ac04ddef71baf44b0520c5ccd109ee
AndreaCuriel <acurielipn@hotmail.com>
2022-04-26 17:55:17 -0500

    Prueba 1 de ExplorerService, fallida

---
+++ test/ExplorerService.test.js
@@ -0,0 +1,10 @@
+const ExplorerService = require("./../lib/services/ExplorerService");
+
+describe("ExplorerService test", () => {
+  test("1) test filter of the filterByMission", () => {
+    const explorer = [{ mission: "node" }];
+    const filterExplorer = ExplorerService.filterByMission(explorer, "node");
+
+    expect(filterExplorer.length).toBe(1);
+  });
+});
