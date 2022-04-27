6095211935d37618bff1e155a6adbfc1508ac0c4
AndreaCuriel <acurielipn@hotmail.com>
2022-04-26 20:01:43 -0500

    Cambiar prueba para que falle y verificar en pruebas automaticas

--- test/Reader.test.js
+++ test/Reader.test.js
@@ -4,6 +4,6 @@ describe("Reader test", () => {
   test("1) Reading and getting a objet", () => {
     const explorers = Reader.readJsonFile("explorers.json");
     const explorer = explorers[0].name;
-    expect(explorer).toBe("Woopa1");
+    expect(explorer).toBe("Woopa2");
   });
 });
