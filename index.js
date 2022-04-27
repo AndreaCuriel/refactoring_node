219cae4ac3f3b00e2a6ffd2d26c55b18cc4c1772
AndreaCuriel <acurielipn@hotmail.com>
2022-04-26 19:38:07 -0500

    Index.js para correr la aplicación

---
+++ index.js
@@ -0,0 +1,25 @@
+const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbussService = require("./lib/services/FizzbuzzService");
+
+const explorers = Reader.readJsonFile("explorers.json");
+
+const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+
+const ExplorersLen = ExplorerService.getAmountOfExplorersByMission(
+  explorers,
+  "node"
+);
+console.log(ExplorersLen);
+
+const ExplorersNames = ExplorerService.getExplorersUsernamesByMission(
+  explorers,
+  "node"
+);
+console.log(ExplorersNames);
+
+const explorersInNodeAndFIZZOrBUZZOrFIZZBUUZZTrick = explorersInNode.map(
+  (explorer) => FizzbussService.applyValidationInExplorer(explorer)
+);
+
+console.log(explorersInNodeAndFIZZOrBUZZOrFIZZBUUZZTrick);
