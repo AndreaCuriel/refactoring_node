018ee9dc6ff93d89ef15d7f2d308e2737f2d40b8
AndreaCuriel <acurielipn@hotmail.com>
2022-04-26 22:10:59 -0500

    Usar fix linter

--- test/ExplorerController.test.js
+++ test/ExplorerController.test.js
@@ -1,21 +1,21 @@
 const ExplorerController = require("../lib/controllers/ExplorerController");
 
 describe("ExplorerController test", () => {
-  test("1) Método getExplorersByMission", () => {
-    const List = ExplorerController.getExplorersByMission("node");
+    test("1) Método getExplorersByMission", () => {
+        const List = ExplorerController.getExplorersByMission("node");
 
-    expect(List[0].name).toBe("Woopa1");
-  });
+        expect(List[0].name).toBe("Woopa1");
+    });
 
-  test("2) getExplorersUsernamesByMission Método ", () => {
-    const ListNames = ExplorerController.getExplorersUsernamesByMission("node");
+    test("2) getExplorersUsernamesByMission Método ", () => {
+        const ListNames = ExplorerController.getExplorersUsernamesByMission("node");
 
-    expect(ListNames[0]).toBe("ajolonauta1");
-  });
+        expect(ListNames[0]).toBe("ajolonauta1");
+    });
 
-  test("3) getExplorersAmonutByMission Método ", () => {
-    const NumAmonut = ExplorerController.getExplorersAmonutByMission("node");
+    test("3) getExplorersAmonutByMission Método ", () => {
+        const NumAmonut = ExplorerController.getExplorersAmonutByMission("node");
 
-    expect(typeof NumAmonut).toBe("number");
-  });
+        expect(typeof NumAmonut).toBe("number");
+    });
 });
